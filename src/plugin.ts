2626959d2d0e234cd2d53c83e3a1e58bee9f30b7
Dylan Awalt-Conley <web@dylan.ac>
2026-02-09 20:48:31 -0500

    feat: implement paged navigation for web snapshots

--- src/plugin.ts
+++ src/plugin.ts
@@ -33,17 +33,24 @@ export class Plugin {
 
   async startup(): Promise<void> {
     this.addToAllWindows();
-    this.registerObserver();
-    await this.styleExistingTabs();
+    Zotero.Reader.registerEventListener(
+      'renderToolbar',
+      this.onRenderToolbar,
+      this.id,
+    );
+    await this.handleExistingTabs();
   }
 
   shutdown(): void {
     this.removeFromAllWindows();
-    this.unregisterObserver();
+    Zotero.Reader.unregisterEventListener(
+      'renderToolbar',
+      this.onRenderToolbar,
+    );
   }
 
   addToWindow(window: _ZoteroTypes.MainWindow): void {
-    this.addMenuItems(window);
+    // this.addMenuItems(window);
   }
 
   addToAllWindows(): void {
@@ -54,7 +61,7 @@ export class Plugin {
   }
 
   removeFromWindow(window: _ZoteroTypes.MainWindow): void {
-    this.removeMenuItems(window);
+    // this.removeMenuItems(window);
   }
 
   removeFromAllWindows(): void {
@@ -64,34 +71,22 @@ export class Plugin {
     });
   }
 
-  async attachStylesToReader(reader: _ZoteroTypes.ReaderInstance) {
-    await reader._waitForReader();
-    await reader._initPromise;
-    const doc = reader?._iframeWindow?.document;
-    if (!doc || !doc.documentElement) {
-      this.log(`couldn't attach styles; tab ${reader.tabID} not ready`);
-      return;
-    }
-    if (doc.getElementById(this.stylesId)) {
-      this.log(`skipping ${reader.tabID}: styles already attached`);
-      return;
-    }
-    const styles = doc.createElement('style');
-    styles.id = this.stylesId;
-    styles.innerText = pluginCss;
-    doc.documentElement.appendChild(styles);
-    this.log('appended styles to tab: ' + reader.tabID);
-  }
-
-  async styleExistingTabs() {
+  async handleExistingTabs() {
     this.log('adding styles to existing tabs');
-    const readers = Zotero.Reader._readers;
+    const readers = Zotero.Reader._readers.filter(isSnapshotReader);
     this.log(
-      `found ${readers.length} reader tags: ${readers.map((r) => r.tabID).join(', ')}`,
+      `found ${readers.length} snapshot reader tabs: ${readers.map((r) => r.tabID).join(', ')}`,
     );
-    await Promise.all(readers.map((r) => this.attachStylesToReader(r)));
+    await Promise.all(readers.map((r) => this.attachButtonsToReader(r)));
     this.log('done adding styles to existing tabs');
   }
+
+  onRenderToolbar = (e: _ZoteroTypes.Reader.EventParams<'renderToolbar'>) => {
+    const reader = e.reader;
+    if (isSnapshotReader(reader)) {
+      this.attachButtonsToReader(reader);
+    }
+  };
 
   #observerID?: string;
   registerObserver() {
@@ -108,7 +103,7 @@ export class Plugin {
             await Promise.all(
               tabIDs.map(async (id) => {
                 const reader = Zotero.Reader.getByTabID(id.toString());
-                await this.attachStylesToReader(reader);
+                // await this.attachStylesToReader(reader);
               }),
             );
           }
@@ -125,6 +120,88 @@ export class Plugin {
       Zotero.Notifier.unregisterObserver(this.#observerID);
       this.#observerID = undefined;
     }
+  }
+
+  #updatedTabs = new Set<string | number>();
+  async attachButtonsToReader(
+    reader: _ZoteroTypes.ReaderInstance<'snapshot'>,
+  ): Promise<void> {
+    if (this.#updatedTabs.has(reader.tabID)) {
+      return;
+    }
+    await reader._waitForReader();
+    await reader._initPromise;
+    const doc = reader?._iframeWindow?.document;
+    const iframe = doc?.querySelector<HTMLIFrameElement>(
+      '.primary-view iframe',
+    );
+    if (!doc || !iframe || !isIframe(iframe)) {
+      this.log(`couldn't attach styles; tab ${reader.tabID} not ready`);
+      return;
+    }
+    const win = await Plugin.getIFrameWindow(iframe);
+    if (!win) {
+      this.log(`couldn't get iframe window for tab ${reader.tabID}`);
+      return;
+    }
+    const toolbarSection = doc.querySelector('div.toolbar .start');
+    if (!toolbarSection) {
+      this.log(`couldn't get toolbar for tab ${reader.tabID}`);
+      return;
+    }
+    if (
+      toolbarSection.querySelector('#next') ||
+      toolbarSection.querySelector('#previous')
+    ) {
+      this.log(`aborting; already added buttons to tab ${reader.tabID}`);
+      return;
+    }
+
+    const btnNext = this.getButton(doc, 'next');
+    const btnPrev = this.getButton(doc, 'previous');
+    btnNext.addEventListener('click', () => win.scrollByPages(1));
+    btnPrev.addEventListener('click', () => win.scrollByPages(-1));
+    toolbarSection.appendChild(btnPrev);
+    toolbarSection.appendChild(btnNext);
+
+    this.#updatedTabs.add(reader.tabID);
+  }
+
+  getButton(doc: Document, type: 'next' | 'previous'): HTMLButtonElement {
+    // Create button
+    const button = doc.createElement('button');
+    button.classList.add('toolbar-button', 'pageUp');
+    button.tabIndex = -1;
+    // button.setAttribute("aria-describedby", "numPages");
+
+    // Create SVG
+    const svgNS = 'http://www.w3.org/2000/svg';
+    const svg = doc.createElementNS(svgNS, 'svg');
+    svg.setAttribute('xmlns', svgNS);
+    svg.setAttribute('width', '20');
+    svg.setAttribute('height', '20');
+    svg.setAttribute('fill', 'none');
+
+    // Create path
+    const path = doc.createElementNS(svgNS, 'path');
+    path.setAttribute('fill', 'currentColor');
+
+    // Distinguish next/previous
+    if (type === 'next') {
+      button.title = 'Next Page';
+      button.id = 'next';
+      path.setAttribute('d', 'm17.116 6 .884.884-8 8-8-8L2.884 6 10 13.116z');
+    } else {
+      button.title = 'Previous Page';
+      button.id = 'previous';
+      path.setAttribute('d', 'M2.884 14 2 13.116l8-8 8 8-.884.884L10 6.884z');
+    }
+
+    // Assemble
+    svg.appendChild(path);
+    button.appendChild(svg);
+
+    return button;
   }
 
   addMenuItems(window: _ZoteroTypes.MainWindow): void {
@@ -175,6 +252,27 @@ export class Plugin {
   }
 
   log(msg: string) {
-    Zotero.debug(`[${config.addonName}] ${msg}`);
+    const message = `[${config.addonName}] ${msg}`;
+    Zotero.debug(message);
+    Zotero.log(message);
+  }
+
+  static getIFrameWindow(iframe: HTMLIFrameElement): Promise<Window | null> {
+    return new Promise((resolve) => {
+      if (iframe?.contentWindow) {
+        resolve(iframe.contentWindow);
+      } else {
+        iframe.addEventListener('load', () => {
+          resolve(iframe.contentWindow || null);
+        });
+      }
+    });
   }
 }
+
+const isIframe = (e: Element): e is HTMLIFrameElement =>
+  e.tagName.toUpperCase() === 'IFRAME';
+
+const isSnapshotReader = (
+  r: _ZoteroTypes.ReaderInstance,
+): r is _ZoteroTypes.ReaderInstance<'snapshot'> => r.type === 'snapshot';
