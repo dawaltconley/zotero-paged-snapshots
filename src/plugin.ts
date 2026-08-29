2ba6ed72562fffcfdf9ea7efbda1f7cc39ea5e1c
Dylan Awalt-Conley <web@dylan.ac>
2026-08-29 15:39:37 -0400

    refactor: separate getButton into its own function

--- src/plugin.ts
+++ src/plugin.ts
@@ -98,51 +98,14 @@ export class Plugin {
       return;
     }
 
-    const btnNext = this.getButton(doc, 'next');
-    const btnPrev = this.getButton(doc, 'previous');
+    const btnNext = getButton(doc, 'next');
+    const btnPrev = getButton(doc, 'previous');
     btnNext.addEventListener('click', () => win.scrollByPages(1));
     btnPrev.addEventListener('click', () => win.scrollByPages(-1));
     toolbarSection.appendChild(btnPrev);
     toolbarSection.appendChild(btnNext);
 
     this.#updatedTabs.add(reader.tabID);
-  }
-
-  getButton(doc: Document, type: 'next' | 'previous'): HTMLButtonElement {
-    // Create button
-    const button = doc.createElement('button');
-    button.classList.add('toolbar-button', 'pageUp');
-    button.tabIndex = -1;
-    // button.setAttribute("aria-describedby", "numPages");
-
-    // Create SVG
-    const svgNS = 'http://www.w3.org/2000/svg';
-    const svg = doc.createElementNS(svgNS, 'svg');
-    svg.setAttribute('xmlns', svgNS);
-    svg.setAttribute('width', '20');
-    svg.setAttribute('height', '20');
-    svg.setAttribute('fill', 'none');
-
-    // Create path
-    const path = doc.createElementNS(svgNS, 'path');
-    path.setAttribute('fill', 'currentColor');
-
-    // Distinguish next/previous
-    if (type === 'next') {
-      button.title = 'Next Page';
-      button.id = 'next';
-      path.setAttribute('d', 'm17.116 6 .884.884-8 8-8-8L2.884 6 10 13.116z');
-    } else {
-      button.title = 'Previous Page';
-      button.id = 'previous';
-      path.setAttribute('d', 'M2.884 14 2 13.116l8-8 8 8-.884.884L10 6.884z');
-    }
-
-    // Assemble
-    svg.appendChild(path);
-    button.appendChild(svg);
-
-    return button;
   }
 
   log(msg: string) {
@@ -162,4 +125,44 @@ export class Plugin {
       }
     });
   }
+}
+
+function getButton(
+  doc: Document,
+  type: 'next' | 'previous',
+): HTMLButtonElement {
+  // Create button
+  const button = doc.createElement('button');
+  button.classList.add('toolbar-button', 'pageUp');
+  button.tabIndex = -1;
+  // button.setAttribute("aria-describedby", "numPages");
+
+  // Create SVG
+  const svgNS = 'http://www.w3.org/2000/svg';
+  const svg = doc.createElementNS(svgNS, 'svg');
+  svg.setAttribute('xmlns', svgNS);
+  svg.setAttribute('width', '20');
+  svg.setAttribute('height', '20');
+  svg.setAttribute('fill', 'none');
+
+  // Create path
+  const path = doc.createElementNS(svgNS, 'path');
+  path.setAttribute('fill', 'currentColor');
+
+  // Distinguish next/previous
+  if (type === 'next') {
+    button.title = 'Next Page';
+    button.id = 'next';
+    path.setAttribute('d', 'm17.116 6 .884.884-8 8-8-8L2.884 6 10 13.116z');
+  } else {
+    button.title = 'Previous Page';
+    button.id = 'previous';
+    path.setAttribute('d', 'M2.884 14 2 13.116l8-8 8 8-.884.884L10 6.884z');
+  }
+
+  // Assemble
+  svg.appendChild(path);
+  button.appendChild(svg);
+
+  return button;
 }
